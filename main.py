300e563a2fd97ee6b147e47e8d91db23da5d9584
Ruben Vermaat <rubenvermaat01@gmail.com>
2024-02-05 11:21:06 +0100

    Code orderning

--- main.py
+++ main.py
@@ -36,36 +36,22 @@ async def on_startup():
     print(f"This bot is owned by {bot.owner}")
     prefixed_commands.setup(bot)
 
-# You are the {member.memberCount} member to join.
-@listen
-async def on_member_join(member):
-    print("Ready")
-    await bot.get_channel(os.getenv('CHANNEL_ID')).send(f"Hello, , welcome to the discord server! You are the member to join")
-#    true_member_count = len([m for m in bot.get_guild(os.getenv('CHANNEL_ID')).members if not m.bot]) # doesn't include bots 
-#    await bot.get_channel(os.getenv('CHANNEL_ID')).send(f"Hello, {member.mention}, welcome to the discord server! You are the {true_member_count} member to join")
-
 @listen()
 async def on_guild_join(event: MemberAdd):
    true_member_count = len([m for m in event.guild.members if not m.bot]) # doesn't include bots 
    random_quote = functions.getRandomQuote()
-   await bot.get_channel(os.getenv('CHANNEL_ID')).send(f"Hello, {event.member.mention}, welcome to the discord server! You are the {true_member_count} member to join\n'" + random_quote.data + "' - " + random_quote.name)
+   await bot.get_channel(os.getenv('CHANNEL_ID')).send(f"Hello, {event.member.mention}, welcome to the discord server! You are the {true_member_count} member to join")
+   await bot.get_channel(os.getenv('CHANNEL_ID')).send("'" + random_quote.data + "' - " + random_quote.name)
 
-@listen()
-async def on_guild_create(event):
-    print(f"guild created : {event.guild.name}")
-
+# @listen()
+# async def on_guild_create(event):
+#     print(f"guild created : {event.guild.name}")
 
 # Message content is a privileged intent.
 # Ensure you have message content enabled in the Developer Portal for this to work.
-@listen()
-async def on_message_create(event):
-    print(f"message received: {event.message.content}")
-
-
-@listen()
-async def on_component(event: Component):
-    ctx = event.ctx
-    await ctx.edit_origin("test")
+# @listen()
+# async def on_message_create(event):
+#     print(f"message received: {event.message.content}")
 
 # Loading all the extension files in the folder 'files'
 # Note: might break if other kind of files/folders exist in folder
